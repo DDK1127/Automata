c0f4345c8a51a7088fe9bbbba01c1705e749ce1d
ddk1127 <derek61003@gmail.com>
2024-03-05 00:48:29 +0800

    more and more

--- hello.go
+++ hello.go
@@ -3,4 +3,16 @@ package main
 
 	func main(){
 		fmt.Printf("Hello world!, 你好，世界,こんにちはせかい\n")
+
+		ddk, wall, bell := "董承恩", "少華", "鍾哥"
+		eric := "eric"
+		fmt.Println(ddk, wall, bell, eric)
+
+		const Pi float64 = 3.1415926
+		const c complex64 = 5+5i
+		const prefix = "ggelq_..."
+
+		var is_active bool // Default is False.
+		is_active = true
+		fmt.Println("now is active?\nANS is = ", prefix, is_active)
 	}
